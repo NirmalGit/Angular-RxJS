cba1e35e11f3546a180a006fca3aa17c6ff17086
NirmalGit <nirsha2007@gmail.com>
2022-10-05 08:19:49 +0530

    feat: product list page

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,7 +1,24 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
-import { catchError, EMPTY, Observable, of, Subscription } from 'rxjs';
+import {
+  catchError,
+  combineLatest,
+  EMPTY,
+  filter,
+  map,
+  Observable,
+  of,
+  startWith,
+  Subject,
+  Subscription,
+} from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -9,25 +26,63 @@ import { ProductService } from './product.service';
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories: ProductCategory[] = [];
+  // categories: ProductCategory[] = [];
+  categories$ = this.productCategoriesService.productCategories$;
+  private categorySelectedSubject = new Subject<number>();
+  categorySelectedId$ = this.categorySelectedSubject.asObservable();
+
+  selectedCategoryId = 1;
 
   // products: Product[] = [];
-  products$ = this.productService.product$.pipe(
-    catchError(err =>{ 
-      this.errorMessage = err ;
-       return EMPTY
+
+  // products$ = this.productService.productsWithCategory$.pipe(
+  //   catchError((err) => {
+  //     this.errorMessage = err;
+  //     return EMPTY;
+  //   })
+  // );
+
+  products$ = combineLatest([
+    this.productService.productsWithCategory$,
+    this.categorySelectedId$.pipe(
+      startWith(0)
+    ),
+  ]).pipe(
+    map(([products, selectedCategoryId]) =>
+      products.filter((product) =>
+        selectedCategoryId ? product.categoryId === selectedCategoryId : true
+      )
+    ),
+    catchError((err) => {
+      this.errorMessage = err;
+      return EMPTY;
     })
-  )
+  );
+
   // sub!: Subscription;
 
-  constructor(private productService: ProductService) { }
+  //productsSimpleFilters
+  productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+    map((products) =>
+      products.filter((product) => {
+        return this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true;
+      })
+    )
+  );
 
- /* ngOnInit(): void {
+  constructor(
+    private productService: ProductService,
+    private productCategoriesService: ProductCategoryService
+  ) {}
+
+  /* ngOnInit(): void {
     // this.sub = this.productService.getProducts()
     //   .subscribe({
     //     next: products => this.products = products,
@@ -50,6 +105,9 @@ export class ProductListComponent {
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    //console.log('Not yet implemented');
+    // this.selectedCategoryId = +categoryId;
+    // this.categorySelectedId$.next(+categoryId);
+    this.categorySelectedSubject.next(+categoryId);
   }
 }
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { catchError, map, Observable, tap, throwError } from 'rxjs';
+import {
+  catchError,
+  combineLatest,
+  map,
+  Observable,
+  tap,
+  throwError,
+} from 'rxjs';
 
 import { Product } from './product';
 import { ProductCategoryService } from '../product-categories/product-category.service';
@@ -14,18 +21,35 @@ export class ProductService {
   private suppliersUrl = 'api/suppliers';
 
   product$ = this.http.get<Product[]>(this.productsUrl).pipe(
-    map((products) =>
+    // map((products) =>
+    //   products.map(
+    //     (product) =>
+    //       ({
+    //         ...product,
+    //         price: product.price ? product.price * 1.5 : 0,
+    //         searchKey: [product.productName],
+    //       } as Product)
+    //   )
+    // ),
+    tap((data) => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
+
+  productsWithCategory$ = combineLatest([
+    this.product$,
+    this.productCategorySerivce.productCategories$,
+  ]).pipe(
+    map(([products, categories]) =>
       products.map(
         (product) =>
           ({
             ...product,
             price: product.price ? product.price * 1.5 : 0,
+            category: categories.find((c) => product.categoryId === c.id)?.name,
             searchKey: [product.productName],
           } as Product)
       )
-    ),
-    tap((data) => console.log('Products: ', JSON.stringify(data))),
-    catchError(this.handleError)
+    )
   );
 
   constructor(
